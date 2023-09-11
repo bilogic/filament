6a34d538ce1ce84c04a8bb5ce35465316992618a
bilogic <bilogic@users.noreply.github.com>
2023-09-11 16:51:11 +0800

    see list of colors

--- packages/support/src/Concerns/HasColor.php
+++ packages/support/src/Concerns/HasColor.php
@@ -12,6 +12,8 @@ trait HasColor
     protected string | array | Closure | null $color = null;
 
     /**
+     * See available colors https://filamentphp.com/docs/3.x/support/colors
+     *
      * @param  string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | Closure | null  $color
      */
     public function color(string | array | Closure | null $color): static
